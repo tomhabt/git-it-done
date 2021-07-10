0f8f5059b9b0f3251ba941d19636f347e9384d46
thomas.habtemariam <thomas.habtemariam@gmail.com>
2021-07-10 19:45:48 -0400

    language function sucess and all about the web is functional

--- assets/js/single.js
+++ assets/js/single.js
@@ -2,6 +2,22 @@ var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+var getRepoName = function() {
+  // grab repo name from url query string
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+
+  if (repoName) {
+    // display repo name on the page
+    repoNameEl.textContent = repoName;
+
+    getRepoIssues(repoName);
+  } else {
+    // if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
+};
+
 var getRepoIssues = function(repo) {
   // format the github api url
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -76,22 +92,6 @@ var displayWarning = function(repo) {
 
   // append to warning container
   limitWarningEl.appendChild(linkEl);
-};
-
-var getRepoName = function() {
-  // grab repo name from url query string
-  var queryString = document.location.search;
-  var repoName = queryString.split("=")[1];
-
-  if (repoName) {
-    // display repo name on the page
-    repoNameEl.textContent = repoName;
-
-    getRepoIssues(repoName);
-  } else {
-    // if no repo was given, redirect to the homepage
-    document.location.replace("./index.html");
-  }
 };
 
 getRepoName();
